77bb6750f315140d2a4773f3dbde08b4b30933bf
Diamond Lee <diamolee55@gmail.com>
2021-06-05 10:58:19 -0700

    adding CSS & fix handlebars

--- server.js
+++ server.js
@@ -2,25 +2,39 @@
 
 const express = require("express");
 const path = require("path");
+const handlebars = require('express-handlebars');
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //Server Set Uo
 const app = express();
 
-const PORT =process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 //staticly hosting our pages (html)
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //Calling HTML Pages
 
-app.get("/", (req,res) => {
-console.log("Page is here");
-res.sendFile(path.join(__dirname, "views/index.html"));
-})
+//app.get("/", (req,res) => {
+//console.log("Page is here");
+//res.send('index')
+//})
+
+//For Handelbars
+
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', handlebars({
+    layoutsDir: `${__dirname}/views/layouts`,
+}));
+
+app.get('/', (req, res) => {
+    console.log("Page is here");
+    res.render('main', { layout: 'index' });
+});
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
